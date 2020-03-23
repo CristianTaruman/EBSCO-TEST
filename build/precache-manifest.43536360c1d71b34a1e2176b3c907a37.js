07b1c14e4b5c3a1891cde2febec6853e0c6a4f26
CristianT <cris.taruman@gmail.com>
2020-03-23 08:21:51 +0000

    logout form

---
+++ build/precache-manifest.43536360c1d71b34a1e2176b3c907a37.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54775bd040f2891aed8bbfcae40d7edc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "af457c7ae71e98731668",
+    "url": "/static/css/2.b9d33c45.chunk.css"
+  },
+  {
+    "revision": "3e26ddc179a06c98b854",
+    "url": "/static/css/main.222fadf8.chunk.css"
+  },
+  {
+    "revision": "af457c7ae71e98731668",
+    "url": "/static/js/2.ef74f412.chunk.js"
+  },
+  {
+    "revision": "ea87f81d39e9435c17efd8c6d3e1ba86",
+    "url": "/static/js/2.ef74f412.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e26ddc179a06c98b854",
+    "url": "/static/js/main.4e8c050d.chunk.js"
+  },
+  {
+    "revision": "0c772d7489bf26f7899f",
+    "url": "/static/js/runtime-main.f70e14d2.js"
+  },
+  {
+    "revision": "9a76e74e107e6163dc82116b8236c132",
+    "url": "/static/media/login.9a76e74e.svg"
+  }
+]);
